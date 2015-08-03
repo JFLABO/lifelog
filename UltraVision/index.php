4e240aca20f840550ce5d3205a286174c043db02
JFLABO <tech@pjfs.biz>
2015-08-04 01:49:04 +0900

    update

--- UltraVision/index.php
+++ UltraVision/index.php
@@ -130,7 +130,8 @@
               <li class="nav-header">Web関連</li>
               <li class="active"><a href="http://pjfs.biz/tbrp/">旅れぽ</a></li>
               <li><a href="./?">UltraVision</a></li>
-              
+              <li><a href="../Minerva/?">Minerva</a></li>
+			  
               <li class="nav-header">お仕事関連</li>
               <li><a href="http://pjfs.biz/tbrp/ChiMeRa.php?param=JFLABO::ChiMeRa::diary">JFLABO</a></li>
               <li><a href="http://dotinstall.com/home">ドットインストール</a></li>
