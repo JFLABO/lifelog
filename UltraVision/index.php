332129498c95f8f49756b19beb59f71874504469
JFLABO[JP] <tech@pjfs.biz>
2015-08-03 23:19:33 +0900

    Update index.php

--- UltraVision/index.php
+++ UltraVision/index.php
@@ -135,11 +135,12 @@
               <li class="nav-header">お仕事関連</li>
               <li><a href="http://pjfs.biz/tbrp/ChiMeRa.php?param=JFLABO::ChiMeRa::diary">JFLABO</a></li>
               <li><a href="http://dotinstall.com/home">ドットインストール</a></li>
-
+              <li><a href="http://cakephp.jp/">CakePHP</a></li>
+              
               <li class="nav-header">友人関連</li>
               <li><a href="https://plus.google.com/u/0/">Google+</a></li>
               <li><a href="https://twitter.com/JFLABO">twitter</a></li>
-              <li><a href="https://github.com/JFLABO/lifelog">lifelog</a></li>
+              <li><a href="https://github.com/JFLABO/lifelog">github:lifelog</a></li>
             </ul>
           </div><!--/.well -->
         </div><!--/span-->
