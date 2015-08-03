4d2c6d81735615df418134f8d1fd5c7807dcd049
JFLABO[JP] <tech@pjfs.biz>
2015-08-03 23:33:57 +0900

    Update index.php

--- UltraVision/index.php
+++ UltraVision/index.php
@@ -157,19 +157,3 @@
     <!-- Le javascript
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
-    <script src="../system/bootstrap/js/jquery.js"></script>
-    <script src="../system/bootstrap/js/bootstrap-transition.js"></script>
-    <script src="../system/bootstrap/js/bootstrap-alert.js"></script>
-    <script src="../system/bootstrap/js/bootstrap-modal.js"></script>
-    <script src="../system/bootstrap/js/bootstrap-dropdown.js"></script>
-    <script src="../system/bootstrap/js/bootstrap-scrollspy.js"></script>
-    <script src="../system/bootstrap/js/bootstrap-tab.js"></script>
-    <script src="../system/bootstrap/js/bootstrap-tooltip.js"></script>
-    <script src="../system/bootstrap/js/bootstrap-popover.js"></script>
-    <script src="../system/bootstrap/js/bootstrap-button.js"></script>
-    <script src="../system/bootstrap/js/bootstrap-collapse.js"></script>
-    <script src="../system/bootstrap/js/bootstrap-carousel.js"></script>
-    <script src="../system/bootstrap/js/bootstrap-typeahead.js"></script>
-
-  </body>
-</html>
