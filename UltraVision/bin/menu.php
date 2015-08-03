205785258cbbb6810cc1b4017d177db3c1e8a35e
JFLABO <tech@pjfs.biz>
2015-08-04 01:51:58 +0900

    mod menu

--- UltraVision/bin/menu.php
+++ UltraVision/bin/menu.php
@@ -1,3 +1,4 @@
-              <li><a href='../?'>UltraVision</a></li>
+              <li><a href='./?'>UltraVision</a></li>
+              <li><a href='../Minerva/?'>Minerva</a></li>
               <li><a href='../?'>index</a></li>
               
