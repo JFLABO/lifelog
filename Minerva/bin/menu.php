649e07a048232d66c925aa3d94309522dd86e663
JFLABO <tech@pjfs.biz>
2015-08-04 07:35:37 +0900

    modi

--- Minerva/bin/menu.php
+++ Minerva/bin/menu.php
@@ -1,4 +1,5 @@
               <li><a href='../UltraVision/?'>UltraVision</a></li>
               <li><a href='./?'>Minerva</a></li>
+              <li><a href='../HyperNote/?'>HyperNote</a></li>
               <li><a href='../?'>index</a></li>
               
--- UltraVision/bin/menu.php
+++ UltraVision/bin/menu.php
@@ -1,4 +1,5 @@
               <li><a href='./?'>UltraVision</a></li>
               <li><a href='../Minerva/?'>Minerva</a></li>
+              <li><a href='../HyperNote/?'>HyperNote</a></li>
               <li><a href='../?'>index</a></li>
               
