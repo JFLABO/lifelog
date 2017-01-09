f594e4dbe02f64acc1e0d67292a65416a61f151d
JFLABO[JP] <tech@pjfs.biz>
2017-01-09 20:12:02 +0900

    Update config.php
    /var/www/htmlに変更

--- system/config.php
+++ system/config.php
@@ -7,7 +7,7 @@
 $file_char_code="UTF-8";
 $output_char_code='UTF-8';
 
-$repo_dir="/var/www/lifelog/DATA/";
-$img_repo="/var/www/lifelog/DATA/org/picture/";
-$txt_dir="/var/www/lifelog/DATA/org/txt/";
+$repo_dir="/var/www/html/lifelog/DATA/";
+$img_repo="/var/www/html/lifelog/DATA/org/picture/";
+$txt_dir="/var/www/html/lifelog/DATA/org/txt/";
 ##############################################################################################
